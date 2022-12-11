71108521ccb19fc2fc85e88935f3ebc60b00a14e
ganzsz <hansderooij3@gmail.com>
2022-12-11 15:55:09 +0100

    Day10: Complete

--- 2022/10/day10.py
+++ 2022/10/day10.py
@@ -4,15 +4,31 @@ instructionSet = {
 }
 
 
+def printTerm(term):
+    for r in term:
+        print(r)
+
+
 with open('/home/hans/Projects/aoc/2022/10/input.real') as f:
     regX = 1
-    cycle = 0
-    out = 0
+    cycle = 1
+    rowPos = 0
+    term = [""]
+    row = 0
     for r in f.readlines():
         r = r.strip().split()
         for _ in range(0, instructionSet[r[0]][1]):
+            if rowPos in range(regX-1, regX+2):
+                term[row] += '#'
+            else:
+                term[row] += '.'
+
+            if (cycle) % 40 == 0:
+                rowPos = -1
+                row+=1
+                term.append('')
             cycle += 1
-            if cycle == 20 or (cycle-20) % 40 == 0:
-                out += cycle * regX
+            rowPos += 1
         regX = instructionSet[r[0]][0](regX, int(r[1]) if len(r)>1 else None )
-    print(out)
+    printTerm(term)
+
