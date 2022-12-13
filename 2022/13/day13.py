6881164c04c0f0edbf79c92532dcb66de62b87f5
Hans de Rooij <ganzsz555@gmail.com>
2022-12-13 23:06:29 +0100

    Day13: Does'nt work

--- 2022/13/day13.py
+++ 2022/13/day13.py
@@ -1,30 +1,49 @@
 import json
-from typing import List
+
+def compNext(left, right):
+  # next step
+  if len(left) > 1 and len(right) > 1: return compareItems(left[1:], right[1:])
+  # one of both will run out 
+  # they deadlocked
+  if len(left) == len(right) == 1: return None
+  else:
+    return len(left) < len(right)
+
+def compEntry(left, right):
+  if len(left) > 0 and len(right) > 0: return compareItems(left, right)
+  else: return len(right) > len(left)
+
 
 def compareItems(left, right):
-  # print(left, right)
-  if len(left) == 0 and len(right) > 0: return True
-  l = left[0]
-  r = right[0]
-  if type(l) == type(r):
-    if type(l) == int:
-      if l != r: return left < right
-      else: return compareItems(left[1:], right[1:])
-    else: return compareItems(left[1], right[1])
-  
-  if type(left) == List:
-    right[0] = [r]
+  if type(left[0]) == type(right[0]):
+    if type(left[0]) == int:
+      if left[0] != right[0]: return left[0]<right[0]
+      else: return compNext(left, right)
+    else:
+      if len(left[0]) > 0 and len(right[0]) > 0:
+        comp = compareItems(left[0], right[0])
+        if comp == None:
+          return compNext(left, right)
+        else:
+          return comp
+      else:
+        return compNext(left[0], right[0])
+  #one is int
   else:
-    left[0] = [l]
-  return compareItems(left,right)
+    if type(left[0]) == int:
+      left[0]=[left[0]]
+    else:
+      right[0]=[right[0]]
+    return compareItems(left, right)
 
 
-  
 
-
-
-with open('/home/chiron/personal/advent-of-code/2022/13/input.test') as f:
+with open('/home/ubuntu/projects/advent-of-code/2022/13/input.real') as f:
+    out = []
+    i = 1
     for rawPair in f.read().split('\n\n'):
       left, right = [json.loads(x) for x in rawPair.split('\n')]
-      print(left, right)
-      print(compareItems(left,right))
+      comp = compEntry(left, right)
+      if comp: out.append(i)
+      i+=1
+    print(sum(out))
