cbb11ed96e3ab8b0a925a9415eea96b5ba0c58ab
Hans de Rooij <ganzsz555@gmail.com>
2022-12-14 10:42:30 +0100

    Day13: Both solved

--- 2022/13/day13.py
+++ 2022/13/day13.py
@@ -1,3 +1,5 @@
+import copy
+import functools
 import json
 
 def compNext(left, right):
@@ -5,14 +7,19 @@ def compNext(left, right):
   if len(left) > 1 and len(right) > 1: return compareItems(left[1:], right[1:])
   # one of both will run out 
   # they deadlocked
-  if len(left) == len(right) == 1: return None
+  if len(left) == len(right): return None
   else:
     return len(left) < len(right)
 
 def compEntry(left, right):
+  left = copy.deepcopy(left)
+  right = copy.deepcopy(right)
   if len(left) > 0 and len(right) > 0: return compareItems(left, right)
-  else: return len(right) > len(left)
+  else: return  len(left)<len(right)
 
+def compSort(left, right):
+  if compEntry(left, right): return -1
+  else: return 1
 
 def compareItems(left, right):
   if type(left[0]) == type(right[0]):
@@ -22,12 +29,12 @@ def compareItems(left, right):
     else:
       if len(left[0]) > 0 and len(right[0]) > 0:
         comp = compareItems(left[0], right[0])
-        if comp == None:
-          return compNext(left, right)
-        else:
-          return comp
       else:
-        return compNext(left[0], right[0])
+        comp = compNext(left[0], right[0])
+      if comp == None:
+        return compNext(left, right)
+      else:
+        return comp
   #one is int
   else:
     if type(left[0]) == int:
@@ -41,9 +48,16 @@ def compareItems(left, right):
 with open('/home/ubuntu/projects/advent-of-code/2022/13/input.real') as f:
     out = []
     i = 1
+    everything = [[[2]],[[6]]]
     for rawPair in f.read().split('\n\n'):
       left, right = [json.loads(x) for x in rawPair.split('\n')]
+      everything.append(left)
+      everything.append(right)
       comp = compEntry(left, right)
       if comp: out.append(i)
       i+=1
     print(sum(out))
+    sor = sorted(everything, key=functools.cmp_to_key(compSort))
+    [print(s) for s in sor]
+    indexes = [k+1 for k in range(0, len(sor)) if sor[k] in [[[2]], [[6]]]]
+    print(indexes, indexes[0]*indexes[1])
