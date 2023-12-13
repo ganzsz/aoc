9999f68cfa739bae072460c6dce78cc56f9274ca
Hans de Rooij <hansr@rometron.nl>
2023-12-13 17:18:58 +0100

    Day 1 complete

--- 2023/01/day1.py
+++ 2023/01/day1.py
@@ -5,6 +5,24 @@ import re
 with open('input.real') as f:
     input = [r.strip() for r in  f.readlines()]
 
+    repl = [
+        ('one','1'),
+        ('two','2'),
+        ('three','3'),
+        ('four','4'),
+        ('five','5'),
+        ('six','6'),
+        ('seven','7'),
+        ('eight','8'),
+        ('nine','9')]
+    
 
-stripped = [re.sub('[a-z]+', '', r) for r in input]
+def clean(line: str):
+    for (i, r) in repl:
+        line = line.replace(i, i+r+i)
+    return line
+
+stripped = [clean(r) for r in input]
+
+stripped = [re.sub('[a-z]+', '', r) for r in stripped]
 print(sum([int(i[0] + i[-1]) for i in stripped]))
